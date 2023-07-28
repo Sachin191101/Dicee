eb0b383447e989510cdb14f5a08f16f0dd9c51da
Sachin191101 <115578203+Sachin191101@users.noreply.github.com>
2023-07-29 01:40:27 +0530

    Add files via upload

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,25 @@
+var btn=document.querySelector(".btn");
+
+btn.addEventListener('click',()=>{
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+    var randomDiceImage = "dice" + randomNumber1 + ".png";
+    var randomImageSource = randomDiceImage;
+    var images1 = document.querySelectorAll("img")[0];
+    images1.setAttribute("src",randomImageSource);
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomImageSource2 = "dice" + randomNumber2 + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+    if ( randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML = " Player 1 Wins!";
+        
+    }
+    else if (randomNumber1 < randomNumber2){
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    }
+    else {
+        document.querySelector("h1").innerHTML = "Draw!";
+    
+    }
+})
+
